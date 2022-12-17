18bfe226c7115981c91a865b4a901ad232ecd72c
Jujumba <far77578@gmail.com>
2022-12-17 13:13:10 +0100

    Rename Collector.java to Starter.java

--- src/main/java/dev/jujumba/newsfromfaridsenpai/logic/Starter.java
+++ src/main/java/dev/jujumba/newsfromfaridsenpai/logic/Starter.java
@@ -11,13 +11,13 @@ import org.springframework.stereotype.Component;
  * @author Jujumba
  */
 @Component
-public class Collector {
+public class Starter {
     private final Pravda pravda;
     private final PresidentOffice presidentOffice;
     private final UkraineNowTelegram ukraineNowTelegram;
     private final NewsCleaner newsCleaner;
     @Autowired
-    public Collector(Pravda pravda, PresidentOffice presidentOffice, UkraineNowTelegram ukraineNowTelegram, NewsCleaner newsCleaner) {
+    public Starter(Pravda pravda, PresidentOffice presidentOffice, UkraineNowTelegram ukraineNowTelegram, NewsCleaner newsCleaner) {
         this.pravda = pravda;
         this.presidentOffice = presidentOffice;
         this.ukraineNowTelegram = ukraineNowTelegram;
@@ -28,12 +28,14 @@ public class Collector {
     public void collect() {
         Thread newsCleanerThread = new Thread(newsCleaner);
         newsCleanerThread.setDaemon(true);
-        newsCleanerThread.start();
+
         Thread presidentOfficeThread = new Thread(presidentOffice);
         Thread pravdaThread = new Thread(pravda);
         Thread tele = new Thread(ukraineNowTelegram);
-        presidentOfficeThread.start();
-        pravdaThread.start();
+
+//        presidentOfficeThread.start();
+//        pravdaThread.start();
         tele.start();
+//        newsCleanerThread.start();
     }
 }
