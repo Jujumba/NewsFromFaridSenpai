4a12e2ad8e947c7f442f191150349ddb52800953
Jujumba <far77578@gmail.com>
2022-12-18 14:35:03 +0100

    AbstractTelegramParser: Bugfix: Add sleeping

--- src/main/java/dev/jujumba/newsfromfaridsenpai/logic/parsers/telegram/AbstractTelegramParser.java
+++ src/main/java/dev/jujumba/newsfromfaridsenpai/logic/parsers/telegram/AbstractTelegramParser.java
@@ -43,6 +43,7 @@ public abstract class AbstractTelegramParser extends AbstractParser {
 
                 if (hasOccurred(href)) {
                     logger.warn("Unsuitable news has been found");
+                    sleep(getDelay());
                     continue label;
                 }
 
