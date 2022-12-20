4803103c589d60fb679b9dcb5a97c42a7426a94f
Jujumba <far77578@gmail.com>
2022-12-20 10:33:41 +0100

    Starter: Reduce boilerplate with @AllArgsConstructor annotation

--- src/main/java/dev/jujumba/newsfromfaridsenpai/logic/Starter.java
+++ src/main/java/dev/jujumba/newsfromfaridsenpai/logic/Starter.java
@@ -4,8 +4,8 @@ import dev.jujumba.newsfromfaridsenpai.logic.cleaner.NewsCleaner;
 import dev.jujumba.newsfromfaridsenpai.logic.parsers.independents.Pravda;
 import dev.jujumba.newsfromfaridsenpai.logic.parsers.independents.PresidentOffice;
 import dev.jujumba.newsfromfaridsenpai.logic.parsers.telegram.UkraineNowTelegram;
+import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
@@ -15,23 +15,17 @@ import java.util.concurrent.Executors;
  * @author Jujumba
  */
 @Component
+@AllArgsConstructor
 public class Starter {
     private final Pravda pravda;
     private final PresidentOffice presidentOffice;
     private final UkraineNowTelegram ukraineNowTelegram;
     private final NewsCleaner newsCleaner;
-    @Autowired
-    public Starter(Pravda pravda, PresidentOffice presidentOffice, UkraineNowTelegram ukraineNowTelegram, NewsCleaner newsCleaner) {
-        this.pravda = pravda;
-        this.presidentOffice = presidentOffice;
-        this.ukraineNowTelegram = ukraineNowTelegram;
-        this.newsCleaner = newsCleaner;
-    }
 
     @SneakyThrows
     public void collect() {
         ExecutorService service = Executors.newFixedThreadPool(this.getClass().getDeclaredFields().length);
-        service.submit(pravda);
+//        service.submit(pravda);
         service.submit(presidentOffice);
         service.submit(ukraineNowTelegram);
         service.submit(newsCleaner);
