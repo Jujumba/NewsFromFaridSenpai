62a57359751a42e05e447259f4ba4dd144f802e1
Jujumba <far77578@gmail.com>
2022-12-21 12:34:46 +0100

    PresidentOffice: Add date parsing

--- src/main/java/dev/jujumba/newsfromfaridsenpai/logic/parsers/independents/PresidentOffice.java
+++ src/main/java/dev/jujumba/newsfromfaridsenpai/logic/parsers/independents/PresidentOffice.java
@@ -8,6 +8,14 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+import static java.time.Month.*;
+import static java.time.Month.DECEMBER;
+
 /**
  * @author Jujumba
  */
@@ -33,11 +41,37 @@ public class PresidentOffice extends AbstractParser {
                     sleep(getDelay());
                     continue label;
                 }
-
-                News news = new News(title, href);
+                LocalDateTime dateTime = parseDate(elem.getElementsByTag("p").text());
+                News news = new News(title, href, dateTime);
                 newsService.save(news);
             }
             sleep(getDelay());
         }
+    }
+    private LocalDateTime parseDate(String date) {
+        String[] split = date.split(" ");
+        //21 грудня 2022 року - 08:01
+        int day = Integer.parseInt(split[0]);
+        Month month = parseMonth(split[1]);
+        int year = Integer.parseInt(split[2]);
+        return LocalDateTime.of(LocalDate.of(year,month, day), LocalTime.parse(split[5]));
+    }
+
+    private Month parseMonth(String split) {
+        return switch (split) {
+            case "січня" -> JANUARY;
+            case "лютого" -> FEBRUARY;
+            case "березня" -> MARCH;
+            case "квітня" -> APRIL;
+            case "травня" -> MAY;
+            case "червня" -> JUNE;
+            case "липня" -> JULY;
+            case "серпня" -> AUGUST;
+            case "вересеня" -> SEPTEMBER;
+            case "жовтеня" -> OCTOBER;
+            case "листопада" -> NOVEMBER;
+            case "грудня" -> DECEMBER;
+            default -> throw new RuntimeException();
+        };
     }
 }
