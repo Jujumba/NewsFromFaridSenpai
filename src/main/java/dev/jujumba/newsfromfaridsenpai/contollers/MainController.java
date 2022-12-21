2920d6c152df038ffc7a36c374a983fcef773489
Jujumba <far77578@gmail.com>
2022-12-21 15:13:53 +0100

    MainController: Implement basic auth with my views

--- src/main/java/dev/jujumba/newsfromfaridsenpai/contollers/MainController.java
+++ src/main/java/dev/jujumba/newsfromfaridsenpai/contollers/MainController.java
@@ -1,11 +1,15 @@
 package dev.jujumba.newsfromfaridsenpai.contollers;
 
 import dev.jujumba.newsfromfaridsenpai.logic.Starter;
+import dev.jujumba.newsfromfaridsenpai.models.User;
 import dev.jujumba.newsfromfaridsenpai.services.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,5 +29,22 @@ public class MainController {
     public String index(Model model) {
         model.addAttribute("all_news", newsService.findAll());
         return "index.html";
+    }
+    @GetMapping("/auth/signin")
+    public String signin() {
+        return "auth/signin.html";
+    }
+
+    @GetMapping("/auth/signup")
+    public String signup(@ModelAttribute("User") User user) {
+        return "auth/signup.html";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        Authentication auth  = SecurityContextHolder.getContext().getAuthentication(); //fetches the user from current session
+        String name = auth.getName();
+        model.addAttribute("name", name);
+        return "profile.html";
     }
 }
