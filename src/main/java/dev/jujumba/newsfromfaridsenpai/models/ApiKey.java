d12fda0afee68a780f666092970615bdd64bf53b
Jujumba <far77578@gmail.com>
2022-12-21 23:10:10 +0100

    ApiKey: Make the random generating of key`s value

--- src/main/java/dev/jujumba/newsfromfaridsenpai/models/ApiKey.java
+++ src/main/java/dev/jujumba/newsfromfaridsenpai/models/ApiKey.java
@@ -3,6 +3,7 @@ package dev.jujumba.newsfromfaridsenpai.models;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Random;
 
 
 /**
@@ -12,6 +13,10 @@ import javax.persistence.*;
 @Table(name = "api_keys")
 @Data
 public class ApiKey {
+    private static final int leftLimit = 48;
+    private static final int rightLimit = 122;
+    private static final int keyLength = 50;
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,6 +28,16 @@ public class ApiKey {
     private User user;
 
     public ApiKey() {
+        initValue(); //todo: rename
+    }
 
+    private void initValue() {
+        Random random = new Random((long) (3 + Math.random() * 999999999));
+
+        value = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(25)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString(); //todo: optimize solution from net
     }
 }
