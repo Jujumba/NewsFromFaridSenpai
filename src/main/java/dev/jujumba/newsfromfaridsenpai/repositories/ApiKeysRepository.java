d101c4d11834e3b094027736df084bf574d1e427
Jujumba <far77578@gmail.com>
2022-12-21 23:24:54 +0100

    ApiKeysRepository: Create method for fetching all api_keys of certain user

--- src/main/java/dev/jujumba/newsfromfaridsenpai/repositories/ApiKeysRepository.java
+++ src/main/java/dev/jujumba/newsfromfaridsenpai/repositories/ApiKeysRepository.java
@@ -2,7 +2,10 @@ package dev.jujumba.newsfromfaridsenpai.repositories;
 
 import dev.jujumba.newsfromfaridsenpai.models.ApiKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Jujumba
@@ -10,4 +13,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ApiKeysRepository extends JpaRepository<ApiKey, Integer> {
     boolean existsByValue(String value);
+    @Query("from api_keys keys where keys.user_id = ?1")
+    List<ApiKey> findAllByUser(Integer id); //may be simpler
 }
