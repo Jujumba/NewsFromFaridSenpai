7523e3ee525835e33db5219b1587b8f2b67b548a
Jujumba <far77578@gmail.com>
2022-12-21 18:51:01 +0100

    User: Add constraints to fields

--- src/main/java/dev/jujumba/newsfromfaridsenpai/models/User.java
+++ src/main/java/dev/jujumba/newsfromfaridsenpai/models/User.java
@@ -3,6 +3,8 @@ package dev.jujumba.newsfromfaridsenpai.models;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 /**
  * @author Jujumba
@@ -20,8 +22,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "email")
+    @NotEmpty
+    @Size(min = 6, message = "Your email is too short or it`ve been registered already")
     private String email;
     @Column(name = "password")
+    @NotEmpty
+    @Size(min = 5, message = "Your password is too short!")
     private String password;
     public User(String email, String password) {
         this.email = email;
