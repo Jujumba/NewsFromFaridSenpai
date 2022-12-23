abb792528c410ff7e2917c8acff81f1891fcf8f4
Jujumba <far77578@gmail.com>
2022-12-23 16:58:30 +0100

    request.py: Clarify comment

--- src/main/resources/request.py
+++ src/main/resources/request.py
@@ -5,6 +5,6 @@ import requests
 
 url = 'http://localhost:8080/api/v1'
 amount = 3
-api_key = None #todo
+api_key = None #todo: paste your API-key generated in your profile (localhost:8080/profile)
 request = requests.post(url, headers={'Authorization': api_key}, json={'amount': amount})
 print(request.text)
