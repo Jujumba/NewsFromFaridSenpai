0246d4909e6fd7ce73ec6afdea7842245839e7a5
Jujumba <far77578@gmail.com>
2022-12-24 14:56:46 +0100

    UkraineNowTelegram: Add ImagesService param to constructor

--- src/main/java/dev/jujumba/newsfromfaridsenpai/logic/parsers/telegram/UkraineNowTelegram.java
+++ src/main/java/dev/jujumba/newsfromfaridsenpai/logic/parsers/telegram/UkraineNowTelegram.java
@@ -1,6 +1,7 @@
 package dev.jujumba.newsfromfaridsenpai.logic.parsers.telegram;
 
 import dev.jujumba.newsfromfaridsenpai.logic.processing.TextHandler;
+import dev.jujumba.newsfromfaridsenpai.services.ImagesService;
 import dev.jujumba.newsfromfaridsenpai.services.NewsService;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +10,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class UkraineNowTelegram extends AbstractTelegramParser{
-    public UkraineNowTelegram(TextHandler textHandler, NewsService newsService) {
-        super(textHandler, newsService);
+    public UkraineNowTelegram(TextHandler textHandler, NewsService newsService, ImagesService imagesService) {
+        super(textHandler, newsService, imagesService);
         setUrl("https://t.me/s/UkraineNow");
     }
 }
