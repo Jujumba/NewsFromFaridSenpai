c0d80b226a66767bdffd7ede6dda2a1b41b2bcfe
Jujumba <far77578@gmail.com>
2023-02-10 00:08:45 +0100

    request.py: change comments

--- src/main/resources/request.py
+++ src/main/resources/request.py
@@ -1,10 +1,10 @@
 """
-A simple script to test api
+A simple script to test the API
 """
 import requests
 
 url = 'http://localhost:8080/api/v1'
 amount = 3
-api_key = None #todo: paste your API-key generated in your profile (localhost:8080/profile)
+api_key = None  # todo: paste your API-key generated in your profile (https://localhost:8080/profile)
 request = requests.post(url, headers={'Authorization': api_key}, json={'amount': amount})
 print(request.text)
